6afc72514154fd8670d86879c58203b17b5616ec
Julien <j.papazian@student.isartdigital.com>
2019-01-26 00:07:52 +0100

    Timer

---
+++ RangeTaChambre/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField]
+    float RoundTimer; // en seconde
+    [SerializeField]
+    Text timer;
+
+    float MiRound;
+    float ChangingMiRound;
+
+	// Use this for initialization
+	void Start () {
+        MiRound = RoundTimer / 2;
+        ChangingMiRound = MiRound;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChangingMiRound -= Time.deltaTime;
+        int IntMiRound = (int)ChangingMiRound;
+        timer.text = IntMiRound.ToString();
+
+        if (RoundTimer < 0)
+        {
+            //InterventionDaronne;
+        }
+    }
+}
