fd40919792c1bd77543c551a85b3f5efd4390b5d
M_deLaharpe <matthieu.delaharpe@gmail.com>
2019-01-26 01:04:38 +0100

    GameManager

---
+++ RangeTaChambre/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+				instance = FindObjectOfType<GameManager>();
+
+			return instance;
+		}
+	}
+
+	private static GameManager instance;
+
+	int player1Score;
+	int player2Score;
+
+	public event System.Action<int, int> ScoreChanged;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.O))
+			OnPlayer1Scored(5);
+
+		if (Input.GetKeyDown(KeyCode.P))
+			OnPlayer2Scored(5);
+	}
+
+	public void OnPlayer1Scored(int points)
+	{
+		player1Score += points;
+		ScoreChanged?.Invoke(player1Score, player2Score);
+	}
+
+	public void OnPlayer2Scored(int points)
+	{
+		player2Score += points;
+		ScoreChanged?.Invoke(player1Score, player2Score);
+	}
+}
