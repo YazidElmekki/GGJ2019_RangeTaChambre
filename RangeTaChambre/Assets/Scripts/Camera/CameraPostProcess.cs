91832a85b3c1a7d7d52e73c30fb50044f6346846
unknown <y.elmekki@student.isartdigital.com>
2019-01-26 14:20:20 +0100

    added hand drawn post process

---
+++ RangeTaChambre/Assets/Scripts/Camera/CameraPostProcess.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPostProcess : MonoBehaviour
+{
+	[SerializeField]
+	private Material pencilEffectMaterial;
+
+	[SerializeField]
+	private float rotationAngleOffset = 5.0f;
+
+	private float currentRotationAngle = 0.0f;
+
+	[SerializeField]
+	private bool updateAngle = true;
+
+	[SerializeField]
+	private float updateRate = 1.0f;
+
+	private void Start()
+	{
+		StartCoroutine(UpdateAngleRoutine());
+	}
+
+	private IEnumerator UpdateAngleRoutine()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(updateRate);
+			OffsetAngle();
+		}
+	}
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if (pencilEffectMaterial != null)
+		{
+			Graphics.Blit(source, destination, pencilEffectMaterial);
+		}
+	}
+
+	private void OffsetAngle()
+	{
+		if (updateAngle == true)
+		{
+			currentRotationAngle += rotationAngleOffset;
+			pencilEffectMaterial.SetFloat("_RotationAngle", currentRotationAngle);
+		}
+	}
+}
